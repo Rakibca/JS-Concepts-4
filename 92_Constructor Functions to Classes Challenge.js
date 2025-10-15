16ce232af740491dbdb9057d1fee637506ba56c2
Rakibul Islam <rakib.ca@gmail.com>
2025-10-14 21:45:25 -0600

    code update

--- 92_Constructor Functions to Classes Challenge.js
+++ 92_Constructor Functions to Classes Challenge.js
@@ -18,8 +18,22 @@ Rebuild the above Constructor Function as a Class.
 // SEE ANSWER BELOW //
 //////////////////////
 
-
-
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.collectedItemsArr = [];
+  }
+  addItem(item) {
+    this.collectedItemsArr.push(item);
+    console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`);
+  }
+}
 
 const wizard = new Character('Merlin');
-wizard.addItem('a wand');
+const witch = new Character('Hermione');
+
+wizard.addItem('a wand'); // Merlin now has: a wand
+wizard.addItem('a map'); // Merlin now has: a wand, a map
+wizard.addItem('a rope'); // Merlin now has: a wand, a map, a rope
+witch.addItem('a map'); // Hermione now has: a map
+witch.addItem('a rope'); // Hermione now has: a map, a rope
